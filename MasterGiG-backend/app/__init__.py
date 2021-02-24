75d94dcaf2c525c6739d01b49a5b4c9aab4923b3
sufiyan8226 <sufiyan8226@hotmail.com>
2021-02-24 16:45:48 +0800

    Updated and merge branches with streaming subsystem protocol

---
+++ MasterGiG-backend/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+#from flask_cors import CORS
+
+app = Flask(__name__)
+app.config.from_object(Config)
+#CORS(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
