5eb96e4752b6172d37ae2289dd3a7a9dafcef898
igan04 <79855525+igan04@users.noreply.github.com>
2021-03-03 11:02:25 +0800

    Backend user management files - ian
    backend logic and html templates to test for::
    -user registration, login/ logout, view and update profile (missing profile pic)

---
+++ routes.py
@@ -0,0 +1,89 @@
+from flask import render_template, flash, redirect, url_for
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, UpdateProfileForm
+import time
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = current_user
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/time')
+def get_current_time():
+    return time.time()
+    #return [{'time': time.time()}]
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        user.set_display_name(form.username.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('registration.html', title='Register', form=form)
+
+
+@app.route('/updateprofile', methods=['GET', 'POST'])
+def updateprofile():
+    if current_user.is_authenticated:
+        form = UpdateProfileForm()
+
+        if form.validate_on_submit():
+            user = current_user
+            user.set_display_name(form.display_name.data)
+            user.set_bio(form.bio.data)
+            user.set_dob(form.dob.data)
+            db.session.commit()
+            flash('Profile Updated!')
+            return redirect(url_for('myprofile'))
+        return render_template('updateprofile.html', title='My Profile', form=form)
+    return redirect(url_for('index'))
+
+
+@app.route('/myprofile', methods=['GET', 'POST'])
+def myprofile():
+    user = current_user
+    return render_template('myprofile.html', user=user)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
