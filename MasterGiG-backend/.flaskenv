bb819f7b9b43cd151d7ae9e93e7a8f4bf7e237a3
wzynus <e0191599@u.nus.edu>
2021-03-15 15:30:24 +0800

    clean up

--- MasterGiG-backend/.flaskenv
+++
@@ -1,4 +0,0 @@
-FLASK_APP=mastergig.py
-export APP_SETTINGS="config.Config"
-export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/mastergig"
-
--- MasterGiG-backend/static/src/components/Home/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Home = () =>
-    <section>
-        <div className="container text-center">
-            <h1>MasterGiG - A platform for artist to connect with their audience</h1>
-        </div>
-    </section>;
