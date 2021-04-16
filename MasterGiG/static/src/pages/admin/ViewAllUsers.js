07570db585edc0594b3f6308e7abbaab1e1a9b3e
zhengyang15 <56710669+wzynus@users.noreply.github.com>
2021-04-16 16:10:12 +0800

    update admin user, feedback and report pages

--- MasterGiG/static/src/pages/admin/ViewAllUsers.js
+++ MasterGiG/static/src/pages/admin/ViewAllUsers.js
@@ -1,20 +1,101 @@
 
-import React from 'react';
-import { Row, Col, Container } from '@themesberg/react-bootstrap';
 
-import AccordionComponent from "../../components/AccordionComponent";
-import Documentation from "../../components/Documentation";
+import React from "react";
+import { Card, Table, Button, ButtonGroup, Row, Col, InputGroup, Form, Dropdown, Container } from "@themesberg/react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faCog, faCheck, faSearch, faSlidersH } from '@fortawesome/free-solid-svg-icons';
 
 export default () => (
-  <article>
-    <Container className="px-0">
-      <Row className="d-flex flex-wrap flex-md-nowrap align-items-center py-4">
-        <Col className="d-block mb-4 mb-md-0">
-          <h1 className="h2">Users Management</h1>
-          
+
+  <Container className="px-0">
+    <div className="d-lg-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+      <div className="mb-4 mb-lg-0">
+
+        <h1>Users List</h1>
+
+      </div>
+      
+    </div>
+    <div className="table-settings mb-4">
+      <Row className="justify-content-between align-items-center">
+        <Col xs={9} lg={4} className="d-flex">
+          <InputGroup className="me-2 me-lg-3">
+            <InputGroup.Text>
+              <FontAwesomeIcon icon={faSearch} />
+            </InputGroup.Text>
+            <Form.Control type="text" placeholder="Search" />
+          </InputGroup>
+          <Form.Select className="w-50">
+            <option defaultChecked>All</option>
+            <option value="1">Active</option>
+            <option value="2">Banned</option>
+          </Form.Select>
+        </Col>
+        <Col xs={3} lg={8} className="text-end">
+          <Dropdown as={ButtonGroup} className="me-2">
+            <Dropdown.Toggle split as={Button} variant="link" className="text-dark m-0 p-0">
+              <span className="icon icon-sm icon-gray">
+                <FontAwesomeIcon icon={faSlidersH} />
+              </span>
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="dropdown-menu-right">
+              <Dropdown.Item className="fw-bold text-dark">Show</Dropdown.Item>
+              <Dropdown.Item className="d-flex fw-bold">
+                10 <span className="icon icon-small ms-auto"><FontAwesomeIcon icon={faCheck} /></span>
+              </Dropdown.Item>
+              <Dropdown.Item className="fw-bold">20</Dropdown.Item>
+              <Dropdown.Item className="fw-bold">30</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown as={ButtonGroup}>
+            <Dropdown.Toggle split as={Button} variant="link" className="text-dark m-0 p-0">
+              <span className="icon icon-sm icon-gray">
+                <FontAwesomeIcon icon={faCog} />
+              </span>
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="dropdown-menu-right">
+              <Dropdown.Item className="fw-bold text-dark">Show</Dropdown.Item>
+              <Dropdown.Item className="d-flex fw-bold">
+                10 <span className="icon icon-small ms-auto"><FontAwesomeIcon icon={faCheck} /></span>
+              </Dropdown.Item>
+              <Dropdown.Item className="fw-bold">20</Dropdown.Item>
+              <Dropdown.Item className="fw-bold">30</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Col>
       </Row>
+    </div>
+    <Card border="light" className="table-wrapper table-responsive shadow-sm">
+      <Card.Body>
+        <Table hover className="user-table align-items-center">
+          <thead>
+            <tr>
+              <th className="border-bottom">Name</th>
+              <th className="border-bottom">Email</th>
+              <th className="border-bottom">Status</th>
+              <th className="border-bottom">User registered at</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <Card.Link className="d-flex align-items-center">
+                  
+                  <div className="d-block">
+                    <span className="fw-bold">Hello, placeholder data Wong Zheng Yang</span>
+                  </div>
+                </Card.Link>
+              </td>
+              <td>zhengyang@nus.com</td>
+              <td>Active</td>
+              <td>January 20, 2021</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Card.Body>
+    </Card>
 
-    </Container>
-  </article>
+
+  </Container>
+
 );
