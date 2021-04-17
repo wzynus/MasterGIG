0d4d3fa779269e91871ddd3bb7dfafad7569a5b7
zhengyang15 <56710669+wzynus@users.noreply.github.com>
2021-04-17 15:27:20 +0800

    update admin broadcast pages

--- MasterGiG/static/src/pages/admin/BroadcastSelectedUsers.js
+++ MasterGiG/static/src/pages/admin/BroadcastSelectedUsers.js
@@ -1,20 +1,77 @@
 
-import React from 'react';
-import { Row, Col, Container } from '@themesberg/react-bootstrap';
+import React, { useState } from "react";
+import moment from "moment-timezone";
+import Datetime from "react-datetime";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row, Card, Form, Button, InputGroup, Container } from '@themesberg/react-bootstrap';
 
-import AccordionComponent from "../../components/AccordionComponent";
-import Documentation from "../../components/Documentation";
 
-export default () => (
-  <article>
+export const BroadcastSelectedUsers = () => {
+  const [dateTime, setDateTime] = useState("");
+  
+  var yesterday = moment().subtract(1, 'day');
+  var valid = function (current) {
+    return current.isAfter(yesterday);
+  };
+
+  return (
     <Container className="px-0">
-      <Row className="d-flex flex-wrap flex-md-nowrap align-items-center py-4">
-        <Col className="d-block mb-4 mb-md-0">
-          <h1 className="h2">Broadcast to selected users</h1>
-          
-        </Col>
-      </Row>
+      <h1 className="h1">Broadcast to selected users</h1>
+      <Card border="light" className="bg-white shadow-sm mb-4">
+        <Card.Body>
+          <h5 className="mb-4">Broadcast Information</h5>
+          <Form>
+            <Row>
+              <Col className="mb-3">
+                <Form.Group id="firstName">
+                  <Form.Label>System Message</Form.Label>
+                  <Form.Control required type="text" placeholder="Enter system message" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="align-items-center">
+              <Col md={6} className="mb-3">
+                <Form.Group id="date">
+                  <Form.Label>Date</Form.Label>
+                  <Datetime
+                    isValidDate={valid}
+                    onChange={setDateTime}
+                    renderInput={(props, openCalendar) => (
+                      <InputGroup>
+                        <InputGroup.Text><FontAwesomeIcon icon={faCalendarAlt} /></InputGroup.Text>
+                        <Form.Control
+                          required
+                          type="text"
+                          value={dateTime ? moment(dateTime).format("MM/DD/YYYY hh:mm A") : ""}
+                          placeholder="mm/dd/yyyy hh:mm AM/PM"
+                          onFocus={openCalendar}
+                          onChange={() => { }} />
+                      </InputGroup>
+                    )} />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={4} className="mb-3">
+                <Form.Group className="mb-2">
+                  <Form.Label>Select users</Form.Label>
+                  <Form.Select id="state" defaultValue="0">
+                    <option value="0">All Users</option>
+                    
+                  </Form.Select>
+                </Form.Group>
+              </Col>
 
+            </Row>
+            <div className="mt-3">
+              <Button variant="primary" type="submit">Broadcast Message</Button>
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
     </Container>
-  </article>
-);
+  );
+};
+
+export default BroadcastSelectedUsers;
